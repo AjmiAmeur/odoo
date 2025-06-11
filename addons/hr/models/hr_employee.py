6a0420bdf3c7194a034eb4600e7537a95a854203
ameur <ameur.ajmi@gmail.com>
2025-06-11 08:18:57 +0000

    hr: réactiver le champ certificate et mettre à jour la vue pour l'inclure

--- addons/hr/models/hr_employee.py
+++ addons/hr/models/hr_employee.py
@@ -116,13 +116,13 @@ class HrEmployeePrivate(models.Model):
     work_permit_scheduled_activity = fields.Boolean(default=False, groups="hr.group_hr_user")
     work_permit_name = fields.Char('work_permit_name', compute='_compute_work_permit_name', groups="hr.group_hr_user")
     additional_note = fields.Text(string='Additional Note', groups="hr.group_hr_user", tracking=True)
-    # certificate = fields.Selection([
-    #     ('graduate', 'Graduate'),
-    #     ('bachelor', 'Bachelor'),
-    #     ('master', 'Master'),
-    #     ('doctor', 'Doctor'),
-    #     ('other', 'Other'),
-    # ], 'Certificate Level', groups="hr.group_hr_user", tracking=True)
+    certificate = fields.Selection([
+          ('graduate', 'Graduate'),
+          ('bachelor', 'Bachelor'),
+          ('master', 'Master'),
+          ('doctor', 'Doctor'),
+          ('other', 'Other'),
+     ], 'Certificate Level', groups="hr.group_hr_user", tracking=True)
     # certificate = fields.Char("Certificate Level", groups="hr.group_hr_user", tracking=True)
     # le champs diplome_id remplace le champs certificate
     diplome_id = fields.Many2one('hr.diplome', string='Diplôme', groups="hr.group_hr_user", store=True, readonly=False)
--- addons/hr/models/res_users.py
+++ addons/hr/models/res_users.py
@@ -137,7 +137,7 @@ class User(models.Model):
     additional_note = fields.Text(related='employee_id.additional_note', readonly=False, related_sudo=False)
     barcode = fields.Char(related='employee_id.barcode', readonly=False, related_sudo=False)
     pin = fields.Char(related='employee_id.pin', readonly=False, related_sudo=False)
-    # certificate = fields.Char(related='employee_id.certificate', readonly=False, related_sudo=False)
+    certificate = fields.Selection(related='employee_id.certificate', readonly=False, related_sudo=False)
     diplome_id = fields.Many2one(related='employee_id.diplome_id', readonly=False, related_sudo=False)
     study_field = fields.Char(related='employee_id.study_field', readonly=False, related_sudo=False)
     study_school = fields.Char(related='employee_id.study_school', readonly=False, related_sudo=False)
